c0016be247ec6d3d77c55c377b7bae8bb9373980
Jeffery Bennett <meandmybadself@gmail.com>
2017-01-15 14:48:50 -0600

    Can now write to and access boards.

--- README.md
+++ README.md
@@ -7,8 +7,34 @@ WIP for interacting with ADDCO traffic signs via a Mirasan gateway using Node.js
 
 * ```USERNAME``` – Mirasan username
 * ```PASSWORD``` – Mirasan password
+* ```BASE_URL``` – Base URL for interaction (e.g. https://something.addco.com)
+* ```NODE_TLS_REJECT_UNAUTHORIZED``` – Should be set to zero unless you want to install your own local cert.
 
 ## Build commands
 
 * ```npm start``` – Runs system in production mode
 * ```npm run dev``` – Runs system in development mode
+
+## Methods
+
+* ```addMessage(message,divisionId,cmsId)```
+* ```getMessageList(divisionId,cmsId)```
+
+## Source documents
+
+When requesting an area's data via getArea(), the resulting JSON doc looks like this:
+```
+{ Layer:
+ { Coords: 'gbtqGbvvuP{xZj{dA`s{@djq@q~k@_kVn`b@cyVvnCkme@}bM`tCqcTtal@',
+   MIdGrps: [ '10|{}|901', 'uuvyHeF_^o_@yB}WgC' ],
+   MIds: [ '2|5201602|3018' ], // 0|cmsId|divisionId
+   MImgIds: 'kA??????|@',
+   PlIdGrps: [],
+   PlIds: [],
+   PlDetails: [],
+   PgIdGrps: [],
+   PgIds: [],
+   PgDetails: [],
+   Overlays: [] },
+   Weather: null }
+```
--- index.js
+++ index.js
@@ -1,43 +1,32 @@
 
 // Consumes a stream & updates ADDCO traffic signs.
 
-const WarningLightsController = require('./lib/warning-lights')
-const wlc = new WarningLightsController()
+const AddcoController = require('./lib/addco-controller')
+const acc = new AddcoController()
 
 let area
 let board
 
-wlc.authenticate(process.env.USERNAME, process.env.PASSWORD)
+acc.authenticate(process.env.USERNAME, process.env.PASSWORD)
   .then(() => {
     console.log('Authenticated.')
     // Request the information for a given area.
-    wlc.getArea()
+    acc.getArea()
       .then((d) => {
-        console.log('Received area data.')
-        /**
-        { Layer:
-         { Coords: 'gbtqGbvvuP{xZj{dA`s{@djq@q~k@_kVn`b@cyVvnCkme@}bM`tCqcTtal@',
-           MIdGrps: [ '10|{}|901', 'uuvyHeF_^o_@yB}WgC' ],
-           MIds: [ '2|5201602|3018' ],
-           MImgIds: 'kA??????|@',
-           PlIdGrps: [],
-           PlIds: [],
-           PlDetails: [],
-           PgIdGrps: [],
-           PgIds: [],
-           PgDetails: [],
-           Overlays: [] },
-           Weather: null }
-        **/
         area = d
         let boardIds = d.Layer.MIds
         let board = boardIds[0]
-        let boardIdComponents = board.split("|")
+        let boardIdComponents = board.split('|')
         let divisionId = boardIdComponents[2]
         let cmsId = boardIdComponents[1]
 
-        let message = "[pt25o0][jp2][jl2][fo442][sc1]THIS IS[nl1][jl2][fo442][sc1]A SAMPLE[nl1][jl2][fo442][sc1]MESSAGE[nl1][jl2][fo442][sc1]COOL!"
-        wlc.addMessage(message,divisionId,cmsId)
+        acc.getMessageList(divisionId, cmsId)
+          .then((data) => {
+
+          })
+          .catch((e) => {
+
+          })
       })
       .catch((e) => {
         console.log(e)
